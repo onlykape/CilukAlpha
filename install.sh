ff7e7d62a643c8c2dee5bc679464be2770d0715d
onlykape <kpranata141@gmail.com>
2023-01-04 09:52:00 +0700

    CilukAlpha

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+pkg upgrade && pkg update
+pkg install imagemagick git nodejs ffmpeg libwebp mc nano yarn pm2
+rm -rf node_modules 
+yarn
+rm -rf session.json 
+rm -rf store.json
+npm start
+
+echo "All dependencies have been installed, please run the command \"npm start\" to immediately start the script"
