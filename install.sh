ab23334209369a78fe9b26e241c6f3c797d2061d
onlykape <kpranata141@gmail.com>
2023-02-27 15:22:47 +0700

    Ciluk

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+pkg update && pkg update
+pm2 completion install
+pkg install imagemagick git nodejs ffmpeg libwebp mc nano yarn
+npm install pm2 -g && pm2 update
+yarn install
+npm start
+
+echo "All dependencies have been installed, please run the command \"npm start\" to immediately start the script"
